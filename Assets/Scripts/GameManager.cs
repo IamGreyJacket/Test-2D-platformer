90edd4ea7648ccddb4df6b84f4889e2b82827ba5
IamGreyJacket <greyserk2712@gmail.com>
2024-09-17 20:18:35 +0400

    Updated scene
    now it has a roof, and a second floor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,5 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
